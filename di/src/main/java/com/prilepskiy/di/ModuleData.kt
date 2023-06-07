613f44e09f92540f3d003e3b97beadb24ff24fba
Prilepskiy_ae <alexey.prilepskiy@btb.dev>
2023-06-07 17:13:37 +0700

    add di; add retrofitModule

--- di/src/main/java/com/prilepskiy/di/ModuleData.kt
+++ di/src/main/java/com/prilepskiy/di/ModuleData.kt
@@ -1,2 +1,36 @@
 package com.prilepskiy.di
 
+import com.prilepskiy.sdk.data.utils.HeaderInterceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val apiModule = module {
+    fun retrofitService(api_url:String): Retrofit {
+
+        return  Retrofit.Builder()
+            .baseUrl(api_url)
+            .addConverterFactory(GsonConverterFactory.create())
+            .apply {
+                client(
+                    OkHttpClient.Builder()
+                        .addInterceptor(HeaderInterceptor())
+                        .addInterceptor(HttpLoggingInterceptor().apply {
+                            level = HttpLoggingInterceptor.Level.BODY
+                        })
+                        .readTimeout(1, TimeUnit.MINUTES)
+                        .connectTimeout(1, TimeUnit.MINUTES)
+                        .writeTimeout(1, TimeUnit.MINUTES)
+                        .build()
+                )
+            }
+            .build()
+    }
+}
+
+val repositoryModule = module{}
+
+val databaseModule = module {}
---
+++ sdk/src/main/java/com/prilepskiy/sdk/data/response/CategoryResponse.kt
@@ -0,0 +1,7 @@
+package com.prilepskiy.sdk.data.response
+
+data class CategoryResponse(
+    val id: Int,
+    val image_url: String,
+    val name: String
+)
