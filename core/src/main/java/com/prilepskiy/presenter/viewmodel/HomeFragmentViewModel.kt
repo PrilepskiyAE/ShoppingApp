27658769dd03a59a38f9f9c4169ab5d28e8c0325
Prilepskiy_ae <alexey.prilepskiy@btb.dev>
2023-06-08 09:18:35 +0700

    create useCaseModule fix response

--- core/src/main/java/com/prilepskiy/presenter/viewmodel/HomeFragmentViewModel.kt
+++ core/src/main/java/com/prilepskiy/presenter/viewmodel/HomeFragmentViewModel.kt
@@ -4,16 +4,17 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.prilepskiy.data.utils.ActionResult
+import com.prilepskiy.domain.interactors.GetCategoryNetworkUseCase
 import com.prilepskiy.domain.repository.DisheRepository
 
 import kotlinx.coroutines.launch
 
-class HomeFragmentViewModel(private val disheRepository: DisheRepository):ViewModel() {
+class HomeFragmentViewModel(private val getCategoryNetworkUseCase: GetCategoryNetworkUseCase):ViewModel() {
     fun getDishe(){
         viewModelScope.launch {
-            when (val result = disheRepository.getDisheListNetwork()){
+            when (val result = getCategoryNetworkUseCase()){
                 is ActionResult.Success -> {
-                    Log.d(TAG, "getDishe1: ${result.data.dishes.size}")
+                    Log.d(TAG, "getDishe1: ${result.data.size}")
                 }
                 is ActionResult.Error ->{
                     Log.d(TAG, "getDishe2: ${result.errors}")
